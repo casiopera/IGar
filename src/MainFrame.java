11fcdf54619bc41724766750eadc304cb9e65a8a
jon <sasiroot@NA103-US10.iurretalhi.eus>
2018-02-19 10:26:37 +0100

    dialog ok

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -58,7 +58,7 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Dialog dialog=new Dialog();
+        Dialog dialog=new Dialog(this, true);
         MainFrame frame= this;
         dialog.setVisible(true);
         frame.setVisible(false);
